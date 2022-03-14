f1a7504d711d75fc97f65eaab89159f9787de51c
Illia Bodnar <bodnarillia26@gmail.com>
2022-03-14 21:45:27 +0200

    logo loading fixed

--- src/Components/UI/HeaderComponent.js
+++ src/Components/UI/HeaderComponent.js
@@ -1,10 +1,11 @@
 import classes from "./HeaderComponent.module.css";
+import logo from "./img/ghibli-logo.webp"
 
 const HeaderComponent = () => {
   return (
     <div className={classes["background-image"]}>
       <img
-        src="https://u.kanobu.ru/editor/images/38/f776d3c9-329c-4cde-98de-1a9ca7a798bb.webp"
+        src={logo}
         alt="Studio Ghibli logo"
       />
     </div>
